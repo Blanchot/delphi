3f4c126ed78b8e0632946eeae11736d391c756e0
Blanchot <Blanchot@users.noreply.github.com>
2019-05-24 15:12:37 +0200

    More

--- inky_test.py
+++ inky_test.py
@@ -1,6 +1,6 @@
 from inky import InkyPHAT
 inky_display = InkyPHAT("red")
-inky_display.set_border(inky_display.BLACK)
+inky_display.set_border(inky_display.WHITE)
 
 from PIL import Image, ImageFont, ImageDraw
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
@@ -22,6 +22,6 @@ y = (inky_display.HEIGHT / 2) - (h / 2)
 '''
 x= 2
 y= 2
-draw.text((x, y), message, inky_display.WHITE, font)
+draw.text((x, y), message, inky_display.BLACK, font)
 inky_display.set_image(img)
 inky_display.show()
