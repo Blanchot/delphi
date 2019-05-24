0a48bc76d9c09148193dcb2990037293980fbdb9
Blanchot <Blanchot@users.noreply.github.com>
2019-05-24 15:05:47 +0200

    Adjustments

--- inky_test.py
+++ inky_test.py
@@ -15,10 +15,13 @@ from font_source_sans_pro import SourceSansPro
 font= ImageFont.truetype(SourceSansPro, 12)
 
 message = "Small taxes kept the same, \nHelp the common people feel secure \nAnd rectify the state."
+'''
 w, h = font.getsize(message)
 x = (inky_display.WIDTH / 2) - (w / 2)
 y = (inky_display.HEIGHT / 2) - (h / 2)
-
+'''
+x= 2
+y= 2
 draw.text((x, y), message, inky_display.BLACK, font)
 inky_display.set_image(img)
 inky_display.show()
