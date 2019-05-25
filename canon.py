cef54a4b633669bea2b8665afa91ee4e6e56eb4c
Blanchot <Blanchot@users.noreply.github.com>
2019-05-25 20:27:12 +0200

    First version test

--- canon.py
+++ canon.py
@@ -1,3 +1,9 @@
+'''Canon.py Version 1: 25 May 2019'''
+
+import csv
+from datetime import datetime
+
+#-------------------------SET UP INKYPHAT
 from inky import InkyPHAT
 inky_display = InkyPHAT("red")
 inky_display.set_border(inky_display.WHITE)
@@ -6,22 +12,52 @@ from PIL import Image, ImageFont, ImageDraw
 img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
 draw = ImageDraw.Draw(img)
 
-'''
-from font_fredoka_one import FredokaOne
-font = ImageFont.truetype(FredokaOne, 22)
-'''
-
 from font_source_sans_pro import SourceSansPro
 font= ImageFont.truetype(SourceSansPro, 13)
 
-message = "035/1 GATHERING 24/05 AM\nSmall taxes kept the same, \nHelp the common people feel secure \nAnd rectify the state."
-'''
-w, h = font.getsize(message)
-x = (inky_display.WIDTH / 2) - (w / 2)
-y = (inky_display.HEIGHT / 2) - (h / 2)
-'''
+#-------------------------LOAD CSV FILE
+
+with open('canon.csv') as csvfile:
+  readCSV= csv.reader(csvfile, delimiter=';')
+  head_nums= []
+  heads= []
+  app_nums= []
+  pos_negs= []
+  times= []
+  texts= []
+  
+  for row in readCSV:
+    head_num= row[0]
+    head= row[1]
+    app_num= row[2]
+    pos_neg= row[3]
+    time= row[4]
+    text= row[5]   
+    
+    head_nums.append(head_num)
+    heads.append(head)
+    app_nums.append(app_num)
+    pos_negs.append(pos_neg)
+    times.append(time)
+    texts.append(text)
+
+
+#-------------------------CREATE AND DRAW MESSAGE
+def current():
+  nu= datetime.now()
+  nu= nu.strftime('%d/%m %p')
+  td= times.index(nu)
+  text= texts[td]
+  text= text.replace("\\n","\n",1) #need to add this because csv file weirdness (check using repr())
+  topline1= head_nums[td]+ ' ' +heads[td].upper()
+  topline2= ' (' +app_nums[td]+':' +pos_negs[td]+') '+times[td]
+  message=  topline1 + topline2 +'\n'+text
+  return message
+
+message= current()
 x= 2
 y= 2
 draw.text((x, y), message, inky_display.BLACK, font)
 inky_display.set_image(img)
 inky_display.show()
+
