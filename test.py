3f13a578c521602ca628601c769fd4194db009ff
Blanchot <Blanchot@users.noreply.github.com>
2019-05-27 08:13:13 +0200

    Adjustments

--- test.py
+++ test.py
@@ -5,16 +5,19 @@ print('Test: ' + str(datetime.now()))
 
 try:
   from inky import InkyPHAT
+  print('InkyPHAT imported!')
 except:
   print('InkyPHAT not imported!')
 
 try:
   from PIL import Image, ImageFont, ImageDraw
+  print('PIL modules imported!')
 except:
   print('PIL modules not imported!')
 
 try:
   from font_source_sans_pro import SourceSansPro
+  print('Font: SourceSansPro imported!')
 except:
   print('Font not imported!')
 
