2560fa4fd09fb5d79e52a3041f77758948eeae86
Blanchot <Blanchot@users.noreply.github.com>
2019-05-27 08:58:10 +0200

    Now testing switching colors

--- test.py
+++ test.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
 
+'''Test.py: 27 May 2019'''
+
 import csv
 from datetime import datetime
-print('Test: ' + str(datetime.now()))
 
-try:
-  from inky import InkyPHAT
-  print('InkyPHAT imported!')
-except:
-  print('InkyPHAT not imported!')
+#print(datetime.now())
 
-try:
-  from PIL import Image, ImageFont, ImageDraw
-  print('PIL modules imported!')
-except:
-  print('PIL modules not imported!')
+#-------------------------SET UP INKYPHAT
+from inky import InkyPHAT
+inky_display = InkyPHAT("red")
+inky_display.set_border(inky_display.WHITE)
 
-try:
-  from font_source_sans_pro import SourceSansPro
-  print('Font: SourceSansPro imported!')
-except:
-  print('Font not imported!')
+from PIL import Image, ImageFont, ImageDraw
+img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
+draw = ImageDraw.Draw(img)
+
+from font_source_sans_pro import SourceSansPro
+font= ImageFont.truetype(SourceSansPro, 12)
 
 #-------------------------LOAD CSV FILE
 
-with open('/home/pi/delphi/canon_texts.csv') as csvfile:
+with open('/home/pi/delphi/canon_texts.csv') as csvfile: #need full path for cron to work!
   readCSV= csv.reader(csvfile, delimiter=';')
   head_nums= []
   heads= []
@@ -56,12 +53,15 @@ def current():
   td= times.index(nu)
   text= texts[td]
   text= text.replace("\\n","\n",4) #need to add this because csv file weirdness (check using repr())
-  topline1= head_nums[td]+ ' ' +heads[td].upper()
-  topline2= ' [' +app_nums[td]+' ' +pos_negs[td]+'] '+times[td]
-  message=  topline1 + topline2 +'\n'+text
+  topline-a= head_nums[td]+ ' ' +heads[td].upper()
+  topline-b= ' [' +app_nums[td]+' ' +pos_negs[td]+'] '+times[td]
+  topline= topline-a + topline-b +'\n'
+  message= topline, appraisal
   return message
 
-message= current()
-print(message) #Should print to cron.log
-print('End test\n')
-
+topline, appraisal= current()
+#print(message) #Should print to cron.log
+draw.text((0, 0), topline, inky_display.RED, font)
+draw.text((0, 17), appraisal, inky_display.BLACK, font)
+inky_display.set_image(img)
+inky_display.show()
