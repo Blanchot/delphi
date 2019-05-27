8ee520de82b0d597b0953adae41c8c286775426d
Blanchot <Blanchot@users.noreply.github.com>
2019-05-27 07:52:43 +0200

    Testing cron job errors

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+print(f'Test: {datetime.now()}')
+
+try:
+  from inky import InkyPHAT
+except ImportError:
+  print('InkyPHAT not imported!')
+
+try:
+  from PIL import Image, ImageFont, ImageDraw
+except ImportError:
+  print('PIL modules not imported!')
+
+try:
+  from font_source_sans_pro import SourceSansPro
+except ImportError:
+  print('Font not imported!')
+
+
